50ad3f80b1aa872860f6cb77067bc17b11e3df1b
Dany2108 <dvincent.freelance@gmail.com>
2021-12-31 03:03:08 +0100

    [DEV] 1.2

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
-prenom="daniel"
-test=0
 
-case ${prenom} in
-    *da*) test=0;;
-    *) test=1;;
-esac
+if [ "$1" == "--create" ]; then
+    echo "option choisi : $1"
 
-echo $test
+    #Vérifie si $2 est vide
+    nb_machine=1
+    [ "$2" != "" ] && nb_machine=$2
+
+    for i in $(seq 1 $nb_machine);do
+    docker run -tid --name $USER-alpine-$i alpine:latest
+    echo "Machine N°$i crée"
+    done
+
+
+elif [ "$1" == "--drop" ]; then
+    echo "option choisi : $1"
+    docker rm -f $(docker ps -a | grep $USER-alpine | awk '{print $1}')
+    echo "les machines ont été supprimé"
+
+elif [ "$1" == "--infos" ]; then
+    echo "option choisi : $1"
+
+elif [ "$1" == "--start" ]; then
+    echo "option choisi : $1"
+
+elif [ "$1" == "--ansible" ]; then
+    echo "option choisi : $1"
+
+else
+    echo "
+    Options :
+        --create : lancer des conteneurs
+
+        --drop : supprimer les conteneurs
+
+        --infos : infos sur les conteneurs (ip, nom, user, ....)
+
+        --start : redémarrer les conteneurs
+
+        --ansible : déploiement arborescence ansible
+    "
+fi
