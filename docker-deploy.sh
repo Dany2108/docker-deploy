972212934e08f8b7ff22dcaad867dbbbb7940eb4
Dany2108 <dvincent.freelance@gmail.com>
2021-12-31 04:52:20 +0100

    [DEV] 1.4

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -8,12 +8,12 @@ if [ "$1" == "--create" ]; then
     [ "$2" != "" ] && nb_machine=$2
 
     last_id=`docker ps -a --format '{{ .Names }}' | awk -F "-" -v user=$USER '$0 ~ user"-alpine" {print $3}' | sort -r | head -1`
-    echo "Déjà $last_id conteneurs sont lancées"
+    echo "Déjà $last_id conteneurs sont lancés"
 
     for ((i=$(($last_id + 1)); i<$(($nb_machine + $last_id + 1)); i++))
     do
     docker run -tid --name $USER-alpine-$i alpine:latest
-    echo "Conteneur N°$i crée"
+    echo "Conteneur N°$i créé"
     done
 
 
@@ -24,10 +24,37 @@ elif [ "$1" == "--drop" ]; then
 
 elif [ "$1" == "--infos" ]; then
     echo "option choisi : $1"
+    for conteneur in $(docker ps -a | grep $USER-alpine | awk '{print $1}');do
+        docker inspect -f ' => {{.Name}} - {{.NetworkSettings.IPAddress }}' $conteneur
+    done
 
 elif [ "$1" == "--start" ]; then
     echo "option choisi : $1"
-    docker start $(docker ps -a | grep $USER-alpine | awk '{print $13}')
+
+    nb_machine=`docker ps -a --format '{{ .Names }}' | awk -F "-" -v user=$USER '$0 ~ user"-alpine" {print $3}' | sort -r | head -1`
+    echo "ils existent $nb_machine conteneurs"
+    echo "Démarrage en cours ..."
+
+    for ((i=1; i<$(($nb_machine + 1)); i++))
+    do
+    docker start $USER-alpine-$i 
+    echo "Le conteneur $USER-alpine-$i a bien été démarré"
+    done    
+    
+    #docker start $(docker ps -a | grep $USER-alpine | awk '{print $1}')
+
+elif [ "$1" == "--stop" ]; then
+    echo "option choisi : $1"
+
+    nb_machine=`docker ps -a --format '{{ .Names }}' | awk -F "-" -v user=$USER '$0 ~ user"-alpine" {print $3}' | sort -r | head -1`
+    echo "$nb_machine conteneurs sont lancés"
+
+
+    for ((i=1; i<$(($nb_machine + 1)); i++))
+    do
+    docker stop $USER-alpine-$i 
+    echo "Le conteneur $USER-alpine-$i a bien été stoppé"
+    done
 
 elif [ "$1" == "--ansible" ]; then
     echo "option choisi : $1"
@@ -42,6 +69,8 @@ else
         --infos : infos sur les conteneurs (ip, nom, user, ....)
 
         --start : redémarrer les conteneurs
+
+        --stop : arréter les conteneurs
 
         --ansible : déploiement arborescence ansible
     "
