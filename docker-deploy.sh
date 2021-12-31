eb3340845675b74900e5b600e52f8aa896abda47
Dany2108 <dvincent.freelance@gmail.com>
2021-12-31 04:27:41 +0100

    [DEV] 1.3

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -8,29 +8,26 @@ if [ "$1" == "--create" ]; then
     [ "$2" != "" ] && nb_machine=$2
 
     last_id=`docker ps -a --format '{{ .Names }}' | awk -F "-" -v user=$USER '$0 ~ user"-alpine" {print $3}' | sort -r | head -1`
-    echo "le dernier ID est $last_id"
-    last_id2=$(($last_id + 1))
-     echo "le prochain ID est $last_id2"
-    nb_machine=$(($nb_machine + $last_id + 1))
-   
+    echo "Déjà $last_id conteneurs sont lancées"
 
-    for ((i=$last_id2; i<$nb_machine; i++))
+    for ((i=$(($last_id + 1)); i<$(($nb_machine + $last_id + 1)); i++))
     do
     docker run -tid --name $USER-alpine-$i alpine:latest
-    echo "Machine N°$i crée"
+    echo "Conteneur N°$i crée"
     done
 
 
 elif [ "$1" == "--drop" ]; then
     echo "option choisi : $1"
     docker rm -f $(docker ps -a | grep $USER-alpine | awk '{print $1}')
-    echo "les machines ont été supprimé"
+    echo "les conteneurs ont été supprimé"
 
 elif [ "$1" == "--infos" ]; then
     echo "option choisi : $1"
 
 elif [ "$1" == "--start" ]; then
     echo "option choisi : $1"
+    docker start $(docker ps -a | grep $USER-alpine | awk '{print $13}')
 
 elif [ "$1" == "--ansible" ]; then
     echo "option choisi : $1"
